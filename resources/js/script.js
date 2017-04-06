3306abe83b1bf7ccd5a2f2e260990eefdc3f34e2
dfejza <dardan.fejza@stonybrook.edu>
2017-04-06 15:34:09 -0400

    Added GET requests to generate portfolio page

--- resources/js/script.js
+++ resources/js/script.js
@@ -91,12 +91,79 @@ function formatPage() {
 // Parse the json file and enumerate the page based off it's specs
 function loadPage(pageNum){
   currentPage = pageNum;
+
   formatPage();
+
+  // Provide an override for unique pages
+  if(currentPage=='page1')
+    formatPagePortfoilio();
+
   // $.each(json[pageNum], function(i,e){
   //   $('body').append(e.text[selectedLanguage]);
   // });
 }
 
+// Override to format the portfolio page
+// Exercises GET
+function formatPagePortfoilio(){
+  // Fetch
+  homeurl = 'https://api.github.com/users/dfejza';
+  repouri = 'https://api.github.com/users/dfejza/repos';
+  requestJSON( homeurl ,function(json)
+  {
+    // Function returned from the call
+    // Check if valid
+    if(json.message == "Not Found" || username == '') {
+      $('body').html("<h2>No User Info Found</h2>");
+    }
+    else {
+        // else we have a user and we display their info
+        var fullname   = json.name;
+        var username   = json.login;
+        var aviurl     = json.avatar_url;
+        var profileurl = json.html_url;
+        var location   = json.location;
+        var followersnum = json.followers;
+        var followingnum = json.following;
+        var reposnum     = json.public_repos;
+
+        if(fullname == undefined) { fullname = username; }
+
+        var outhtml = '<h2>'+fullname+' <span class="smallname">(@<a href="'+profileurl+'" target="_blank">'+username+'</a>)</span></h2>';
+        outhtml = outhtml + '<div class="ghcontent"><div class="avi"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="80" height="80" alt="'+username+'"></a></div>';
+        outhtml = outhtml + '<p>Followers: '+followersnum+' - Following: '+followingnum+'<br>Repos: '+reposnum+'</p></div>';
+        outhtml = outhtml + '<div class="repolist clearfix">';
+
+        var repositories;
+        $.getJSON(repouri, function(json){
+          repositories = json;   
+          outputPageContent();                
+        });          
+        
+        function outputPageContent() {
+          if(repositories.length == 0) { outhtml = outhtml + '<p>No repos!</p></div>'; }
+          else {
+            outhtml = outhtml + '<p><strong>Repos List:</strong></p> <ul>';
+            $.each(repositories, function(index) {
+              outhtml = outhtml + '<li><a href="'+repositories[index].html_url+'" target="_blank">'+repositories[index].name + '</a></li>';
+            });
+            outhtml = outhtml + '</ul></div>'; 
+          }
+          $('body').append(outhtml);
+        } // end outputPageContent()
+      } // end else statement
+  });
+}
+
+// GET json
+function requestJSON(url, callback) {
+  $.ajax({
+    url: url,
+    complete: function(xhr) {
+      callback.call(null, xhr.responseJSON);
+    }
+  });
+}
 
 
 function saveLanguage(cookieValue) {
