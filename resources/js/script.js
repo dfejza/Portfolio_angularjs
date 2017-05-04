d92b9d7567f89cfb280bd3be5a3b11ce6b68613c
dfejza <dardan.fejza@stonybrook.edu>
2017-05-04 16:23:00 -0400

    Changing the direction of this portfolio
    The previous, automatic generic method, while cool did not allow uniqueness (go figure).
    Changing the method of implementation

--- resources/js/script.js
+++ resources/js/script.js
@@ -67,24 +67,30 @@ $(document).ready(function(){
 // Function used to change the lanuage of the website.
 // Called when the selection from
 function formatPage() {
-      // Change the text found on the language selection
-      //$('#languageText').text(json.selectorText[selectedLanguage]);
+  //First lets remove the old page
+  //TODO
+  
+  // Now lets load the specified page into body
+  $("#main").load("home.html");
 
-      // Cycle through the navigation bar and change the language
-      $('.navigation li').each(function(i,e){
-        $(e).text(json.navigation[i][selectedLanguage]);
-      });
+      // // Change the text found on the language selection
+      // //$('#languageText').text(json.selectorText[selectedLanguage]);
 
-      // Change the contents of the body by redrawing
-      // First remove
-      $('.dynamic').each(function(i,e){
-        $(e).remove();
-      });
-      // Next redraw
-      $.each(json[currentPage], function(i,e){
-        $('body').append(e[selectedLanguage]);
-      });
-      $('.dynamic').addClass('animated bounceinleft');
+      // // Cycle through the navigation bar and change the language
+      // $('.navigation li').each(function(i,e){
+      //   $(e).text(json.navigation[i][selectedLanguage]);
+      // });
+
+      // // Change the contents of the body by redrawing
+      // // First remove
+      // $('.dynamic').each(function(i,e){
+      //   $(e).remove();
+      // });
+      // // Next redraw
+      // $.each(json[currentPage], function(i,e){
+      //   $('body').append(e[selectedLanguage]);
+      // });
+      // $('.dynamic').addClass('animated bounceinleft');
 }
 
 // Make everything dynamic.
@@ -94,92 +100,79 @@ function loadPage(pageNum){
 
   formatPage();
 
-  // Provide an override for unique pages
-  // Homepage Feature removed. Output looks clunky
-  // if(currentPage=='page0')
-  //   formatPageHome();
-  if(currentPage=='page1')
-    formatPagePortfoilio();
+  // // Provide an override for unique pages
+  // // Homepage Feature removed. Output looks clunky
+  // // if(currentPage=='page0')
+  // //   formatPageHome();
+  // if(currentPage=='page1')
+  //   formatPagePortfoilio();
 
-  // $.each(json[pageNum], function(i,e){
-  //   $('body').append(e.text[selectedLanguage]);
-  // });
+  // // $.each(json[pageNum], function(i,e){
+  // //   $('body').append(e.text[selectedLanguage]);
+  // // });
 }
 
-// Feature removed. Output looks clunky
-// // Override for homepage
-// function formatPageHome(){
-//   //URL for readme
-//   homepageurl = 'https://raw.githubusercontent.com/dfejza/portfolio/master/README.md';
-//   // GET request
-//   $.get( homepageurl ,function(json)
+// // Override to format the portfolio page
+// // Exercises GET
+// function formatPagePortfoilio(){
+//   // Fetch
+//   homeurl = 'https://api.github.com/users/dfejza';
+//   repouri = 'https://api.github.com/users/dfejza/repos';
+//   requestJSON( homeurl ,function(json)
 //   {
-//     var outhtml = '<h2>'+ json + '</h2>';
-//     $('body').append(outhtml);
+//     // Function returned from the call
+//     // Check if valid
+//     if(json.message == "Not Found" || username == '') {
+//       $('body').html("<h2>No User Info Found</h2>");
+//     }
+//     else {
+//         // else we have a user and we display their info
+//         var fullname   = json.name;
+//         var username   = json.login;
+//         var aviurl     = json.avatar_url;
+//         var profileurl = json.html_url;
+//         var location   = json.location;
+//         var followersnum = json.followers;
+//         var followingnum = json.following;
+//         var reposnum     = json.public_repos;
+
+//         if(fullname == undefined) { fullname = username; }
+
+//         var outhtml = '<h2> <span class="content_title">'+fullname+' </span> <span class="content_name">(@<a href="'+profileurl+'" target="_blank">'+username+'</a>)</span></h2>';
+//         outhtml = outhtml + '<div class="content_body"><div class="content_pic"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="120" height="120" alt="'+username+'"></a></div>';
+//         outhtml = outhtml + '<p>Followers: '+followersnum+'<br>Following: '+followingnum+'<br>Repos: '+reposnum+'</p><br><br><br></div>';
+//         outhtml = outhtml + '<div class="content_repos">';
+
+//         var repositories;
+//         $.getJSON(repouri, function(json){
+//           repositories = json;   
+//           outputPageContent();                
+//         });          
+        
+//         function outputPageContent() {
+//           if(repositories.length == 0) { outhtml = outhtml + '<p>No repos!</p></div>'; }
+//           else {
+//             outhtml = outhtml + '<p><span class="content_repotitle"><strong>Repos List:</strong></p></span>';
+//             $.each(repositories, function(index) {
+//               outhtml = outhtml + '<li><a href="'+repositories[index].html_url+'" target="_blank">'+repositories[index].name + '</a></li>';
+//             });
+//             outhtml = outhtml + '</div>'; 
+//           }
+//           $('.dynamic').append(outhtml);
+//         } // end outputPageContent()
+//       } // end else statement
 //   });
 // }
 
-// Override to format the portfolio page
-// Exercises GET
-function formatPagePortfoilio(){
-  // Fetch
-  homeurl = 'https://api.github.com/users/dfejza';
-  repouri = 'https://api.github.com/users/dfejza/repos';
-  requestJSON( homeurl ,function(json)
-  {
-    // Function returned from the call
-    // Check if valid
-    if(json.message == "Not Found" || username == '') {
-      $('body').html("<h2>No User Info Found</h2>");
-    }
-    else {
-        // else we have a user and we display their info
-        var fullname   = json.name;
-        var username   = json.login;
-        var aviurl     = json.avatar_url;
-        var profileurl = json.html_url;
-        var location   = json.location;
-        var followersnum = json.followers;
-        var followingnum = json.following;
-        var reposnum     = json.public_repos;
-
-        if(fullname == undefined) { fullname = username; }
-
-        var outhtml = '<h2> <span class="content_title">'+fullname+' </span> <span class="content_name">(@<a href="'+profileurl+'" target="_blank">'+username+'</a>)</span></h2>';
-        outhtml = outhtml + '<div class="content_body"><div class="content_pic"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="120" height="120" alt="'+username+'"></a></div>';
-        outhtml = outhtml + '<p>Followers: '+followersnum+'<br>Following: '+followingnum+'<br>Repos: '+reposnum+'</p><br><br><br></div>';
-        outhtml = outhtml + '<div class="content_repos">';
-
-        var repositories;
-        $.getJSON(repouri, function(json){
-          repositories = json;   
-          outputPageContent();                
-        });          
-        
-        function outputPageContent() {
-          if(repositories.length == 0) { outhtml = outhtml + '<p>No repos!</p></div>'; }
-          else {
-            outhtml = outhtml + '<p><span class="content_repotitle"><strong>Repos List:</strong></p></span>';
-            $.each(repositories, function(index) {
-              outhtml = outhtml + '<li><a href="'+repositories[index].html_url+'" target="_blank">'+repositories[index].name + '</a></li>';
-            });
-            outhtml = outhtml + '</div>'; 
-          }
-          $('.dynamic').append(outhtml);
-        } // end outputPageContent()
-      } // end else statement
-  });
-}
-
-// GET json
-function requestJSON(url, callback) {
-  $.ajax({
-    url: url,
-    complete: function(xhr) {
-      callback.call(null, xhr.responseJSON);
-    }
-  });
-}
+// // GET json
+// function requestJSON(url, callback) {
+//   $.ajax({
+//     url: url,
+//     complete: function(xhr) {
+//       callback.call(null, xhr.responseJSON);
+//     }
+//   });
+// }
 
 
 function saveLanguage(cookieValue) {
